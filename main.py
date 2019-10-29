e0a9a38df68cf771324238b866704fbe7fe79f79
marbieg <45802057+marbieg@users.noreply.github.com>
2019-10-29 12:18:21 +0100

    example showing difference between deep and shallow copy added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello world!")
+
+lista = [1, 2, 3, 4, 5]
+l1 = lista.copy()  # performs deep copy
+l1[0] = 100
+print(lista)
+
+l2 = lista  # performs shallow copy
+l2[0] = 100
+print(lista)
